867dd9a79c7d47c7b5489e5baa193f4462eb8765
Gabrielli Sartori <gabriellisartori@gmail.com>
2023-09-07 15:33:36 -0300

    feat: :sparkles: add establishment_id in daily pizza sale limit

--- backend/app/Services/DailyPizzaSaleLimit/CreateDailyPizzaSaleLimitService.php
+++ backend/app/Services/DailyPizzaSaleLimit/CreateDailyPizzaSaleLimitService.php
@@ -3,12 +3,15 @@
 namespace App\Services\Establishment;
 
 use App\Models\DailyPizzaSaleLimit;
+use App\Models\Establishment;
 
 class CreateDailyPizzaSaleLimitService
 {
     public function handle(array $attributes): DailyPizzaSaleLimit
     {
         $dailyPizzaSaleLimit = new DailyPizzaSaleLimit($attributes);
+        $establishment = Establishment::first();
+        $dailyPizzaSaleLimit->establishment_id = $establishment->id;
 
         $dailyPizzaSaleLimit->save();
 
